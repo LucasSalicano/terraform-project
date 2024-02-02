4566ffccbdb1c858ab244d21855d3af20c290f5b
Lucas Salicano <lucassalicano@hotmail.com>
2024-02-02 17:22:05 -0300

    feat: create https alb listener

--- 2-platform/ecs.tf
+++ 2-platform/ecs.tf
@@ -31,6 +31,21 @@ resource "aws_alb" "ecs_cluster_alb" {
   }
 }
 
+resource "aws_alb_listener" "ecs_alb_https_listener" {
+  load_balancer_arn = aws_alb.ecs_cluster_alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.ecs_domain_certificate.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.ecs_default_target_group.arn
+  }
+
+  depends_on = [aws_alb_target_group.ecs_default_target_group]
+}
+
 resource "aws_alb_target_group" "ecs_default_target_group" {
   name     = "${var.ecs_cluster_name}-default-tg"
   port     = 80
